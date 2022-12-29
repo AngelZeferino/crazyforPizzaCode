73ad403b03740c98f43d3305b5d0e3d26b11edc9
AngelZeferino <120287313+AngelZeferino@users.noreply.github.com>
2022-12-28 20:42:19 -0600

    12/28/22
    Added menu and worked on home

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        amber: colors.amber,
+        emerald: colors.emerald,
+      }
+    },
   },
   plugins: [],
 }
