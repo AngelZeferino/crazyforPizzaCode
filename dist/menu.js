6872b93c8590bbe457f33043c0f0f762b53753d9
AngelZeferino <zeferino.angel9@gmail.com>
2023-01-06 23:48:40 -0600

    01/06/23

--- dist/menu.js
+++ dist/menu.js
@@ -4,8 +4,28 @@ btn.addEventListener('click',() => {
 menu.classList.toggle("hidden");
 });
 
-const btn1= document.querySelector('.starters');
-const menu1= document.querySelector('.salads');
-btn.addEventListener('click',() => {
-menu1.classList.toggle("hidden");
+const toTop =document.querySelector(".to-top");
+
+window.addEventListener("scroll",() =>{
+    if(window.pageYOffset >100){
+        toTop.classList.add("active");
+    }else{
+    toTop.classList.remove("active");
+    }
+})
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 40){
+            $('#topBtn').fadeIn();
+        } else{
+            $('#topBtn').fadeOut();
+        }
+    });
+
+    $("#topBtn").click(function(){
+        $('html ,body').animate({scrollTop : 0},800);
+    });
 });
+
+
